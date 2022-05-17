514beff69843817fe9146c36d6cf229b253d4b34
José Miguel <a19esrujo@iesgrancapitan.org>
2022-05-17 08:48:34 +0200

    fix #2 refactor: mensaje de la excepción

--- src/examenOptimizacionYDocumentacion/EdadErroneaException.java
+++ src/examenOptimizacionYDocumentacion/EdadErroneaException.java
@@ -2,7 +2,7 @@ package examenOptimizacionYDocumentacion;
 
 public class EdadErroneaException extends Exception {
 
-	public EdadErroneaException(String message) {
-		super(message);
+	public EdadErroneaException(String messages) {
+		super(messages);
 	}
 }
